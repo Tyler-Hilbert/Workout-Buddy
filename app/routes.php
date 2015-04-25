6ee3e540f92a6b5822ab321217eff24e324ec62e
Tyler-Hilbert <tylerjoshuahilbert@gmail.com>
2015-04-25 11:54:21 -0400

    Mild fixes

--- app/routes.php
+++ app/routes.php
@@ -18,24 +18,23 @@ Route::get('/', array('as' => 'home', 'uses' => function()
 	return $athlete;
 }));
 
+
 Route::get('athlete', ['as' => 'athlete.create', 'uses' => 'AthletesController@create']);
 Route::post('athlete', ['as' => 'athlete.store', 'uses' => 'AthletesController@store']);
+
 
 Route::get('workout', ['as' => 'workout.create', 'uses' => 'WorkoutsController@create']);
 Route::post('workout', ['as' => 'workout.store', 'uses' => 'WorkoutsController@store']);
 
-<<<<<<< HEAD
+
+Route::get('workout_name', ['as' => 'workout_name.create', 'uses' => 'WorkoutNamesController@create']);
+Route::post('workout_name', ['as' => 'workout_name.store', 'uses' => 'WorkoutNamesController@store']);
+
+
 Route::get('calculator', function() {
 	return View::make('calculator');
 });
 Route::post('getworkout', 'CalcController@show');
-=======
-Route::get('workout_name', ['as' => 'workout_name.create', 'uses' => 'WorkoutNamesController@create']);
-Route::post('workout_name', ['as' => 'workout_name.store', 'uses' => 'WorkoutNamesController@store']);
-
-Route::get('calculator', ['as' => 'calculator.rm', 'uses' => 'HomeController@showCalc']);
->>>>>>> origin/master
-
 
 
 
