ceca046edc9bdd88293055e3efe7c5001df1f138
Tyler-Hilbert <tylerjoshuahilbert@gmail.com>
2015-04-25 12:34:42 -0400

    Fixed ajax

--- app/controllers/CalcController.php
+++ app/controllers/CalcController.php
@@ -1,22 +1,12 @@
 <?php
 
 class CalcController extends \BaseController {
-
-	/**
-	 * Display the specified resource.
-	 *
-	 * @param  int  $id
-	 * @return Response
-	 */
-	public function show($id)
+	public function show()
 	{
-		return "D";
 		$workout = Workout::first();
 		$reps = 10;
 		$weight = $workout->weight;
-
 		return $weight * (1 + ($reps / 30));
 	}
-
 
 }
--- app/views/calculator.blade.php
+++ app/views/calculator.blade.php
@@ -1,18 +1,31 @@
 @extends('layout')
 
 @section('content')
+	<script>
+		function calc() {
+			var perList = document.getElementById("percent");
+			var percent = perList.options[perList.selectedIndex].value;
+	        $.ajax({
+				url: "getworkout",
+				method: "POST"
+			}).done(function(response) {
+				var output = Math.round(percent * response);
+				$("#output").html(output);
+			});
+		}
+	</script>
+
 	<div>
-		<select id='percents'>
+		<select id='percent'>
 			<option value='.75'>75%</option>
 			<option value='.85'>85%</option>
 			<option value='.95'>95%</option>
 		</select>
 	</div>
 
-	{{ HTML::script('js/calculations.js') }}
-
 	<button onClick="calc()">Calculate</button>
 
+	<div id="output"></div>
 
 
 
