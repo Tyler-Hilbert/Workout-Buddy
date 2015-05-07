9e88593b99f6368448753bcb9d1b382526bcfa69
Tyler-Hilbert <tylerjoshuahilbert@gmail.com>
2015-05-06 22:00:25 -0400

    Added front end form validation and style

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -4,6 +4,7 @@
 	<meta charset="UTF-8">
     <script src = "http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>	
     {{ HTML::style("css/bootstrap.min.css") }}
+    {{ HTML::style("css/style.css") }}
 	<title>Workout Buddy</title>
 	@yield('js')
 </head>
--- app/views/workout.blade.php
+++ app/views/workout.blade.php
@@ -3,61 +3,63 @@
 @section('content')
 	<div class="container">
 		<h1>Record Results</h1>
-		<div class="well well-sm">
-			{{ Form::open(array('route' => 'workout.store')) }}
-				<div class="form-group">
-					{{ Form::label('athlete', 'Athlete', ['class' => 'control-label']) }}
-					<?php
-						$athletes = Athlete::select('id', DB::raw('lastname || " " || firstname  AS full_name'))
-									->orderBy('full_name', 'asc')
-									->lists('full_name','id');
-
-						echo Form::select('athlete_id', $athletes);
-					?>
-					@if ($errors->has('athlete_id')) <p class="help-block">{{ $errors->first('athlete_id') }}</p> @endif
-				</div>
-				<div class="form-group">
-					{{ Form::label('workout_date', 'Workout Date', ['class' => 'control-label']) }}
-					{{ Form::input('date', 'workout_date') }}
-					@if ($errors->has('workout_date')) <p class="help-block">{{ $errors->first('workout_date') }}</p> @endif					
-				</div>
-				<div id="cloneable_exercise">
+		<div class="col-md-8">
+			<div class="well well-sm">
+				{{ Form::open(array('route' => 'workout.store')) }}
 					<div class="form-group">
-						{{ Form::label('exercise', 'Exercise', ['class' => 'control-label']) }}
+						{{ Form::label('athlete', 'Athlete', ['class' => 'control-label']) }}
 						<?php
-							$workout = Exercise::orderBy('exercise', 'asc')->lists('exercise','id');
-							echo Form::select('exercise0', $workout);
+							$athletes = Athlete::select('id', DB::raw('lastname || " " || firstname  AS full_name'))
+										->orderBy('full_name', 'asc')
+										->lists('full_name','id');
+
+							echo Form::select('athlete_id', $athletes);
 						?>
+						@if ($errors->has('athlete_id')) <p class="help-block">{{ $errors->first('athlete_id') }}</p> @endif
 					</div>
 					<div class="form-group">
-						{{ Form::label('weight', 'Weight', ['class' => 'control-label']) }}
-						{{ Form::text('weight0', null, ['class' => 'form-control input-lg']) }}
+						{{ Form::label('workout_date', 'Workout Date', ['class' => 'control-label']) }}
+						{{ Form::input('date', 'workout_date', null, ['required']) }}
+						@if ($errors->has('workout_date')) <p class="help-block">{{ $errors->first('workout_date') }}</p> @endif					
 					</div>
-					<div class="form-group">
-						{{ Form::label('reps', 'Reps', ['class' => 'control-label']) }}
-						{{ Form::text('reps0', null, ['class' => 'form-control input-lg']) }}
+					<div id="cloneable_exercise">
+						<div class="form-group">
+							{{ Form::label('exercise', 'Exercise', ['class' => 'control-label']) }}
+							<?php
+								$workout = Exercise::orderBy('exercise', 'asc')->lists('exercise','id');
+								echo Form::select('exercise0', $workout);
+							?>
+						</div>
+						<div class="form-group">
+							{{ Form::label('weight', 'Weight', ['class' => 'control-label']) }}
+							{{ Form::number('weight0', null, ['class' => 'form-control input-sm', 'required']) }}
+						</div>
+						<div class="form-group">
+							{{ Form::label('reps', 'Reps', ['class' => 'control-label']) }}
+							{{ Form::number('reps0', null, ['class' => 'form-control input-sm', 'required', 'type' => 'number']) }}
+						</div>
 					</div>
-				</div>
 
 
-				<script>
-					var cloneCount = 0;
-					var cache = $('#cloneable_exercise').clone();
-					function addExercise() {
-						var cloned = cache.clone().insertBefore('#buttons');
-						cloned.find('[name="weight0"]').attr("name", "weight" + (cloneCount+1));
-						cloned.find('[name="reps0"]').attr("name", "reps" + (cloneCount+1));
-						cloned.find('[name="exercise0"]').attr("name", "exercise" + (cloneCount+1));
-						cloneCount++;
-				    }
-			    </script>
+					<script>
+						var cloneCount = 0;
+						var cache = $('#cloneable_exercise').clone();
+						function addExercise() {
+							var cloned = cache.clone().insertBefore('#buttons');
+							cloned.find('[name="weight0"]').attr("name", "weight" + (cloneCount+1));
+							cloned.find('[name="reps0"]').attr("name", "reps" + (cloneCount+1));
+							cloned.find('[name="exercise0"]').attr("name", "exercise" + (cloneCount+1));
+							cloneCount++;
+					    }
+				    </script>
 
 
-				<div class="form-group" id="buttons">
-					{{ Form::submit('Record Results', ['class' => 'btn btn-primary btn-lg']) }}
-					{{ Form::button('Add Exercise', ['class' => 'btn', 'onClick' => 'addExercise()']) }}
-				</div>
-			{{ Form::close() }}
+					<div class="form-group" id="buttons">
+						{{ Form::submit('Record Results', ['class' => 'btn btn-primary btn-lg']) }}
+						{{ Form::button('Add Exercise', ['class' => 'btn', 'onClick' => 'addExercise()']) }}
+					</div>
+				{{ Form::close() }}
+			</div>
 		</div>
 	</div>	
 @stop 
