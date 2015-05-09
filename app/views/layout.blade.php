22d9981cd816de2f030ac465f4e78269848b2322
ajay-patnaik <patnaikaj@gmail.com>
2015-05-09 12:33:00 -0400

    Fixed navbar collapse issues

--- app/views/layout.blade.php
+++ app/views/layout.blade.php
@@ -2,7 +2,10 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-    <script src = "http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
     {{ HTML::style("css/bootstrap.min.css") }}
     {{ HTML::style("css/style.css") }}
 	<title>Workout Buddy</title>
